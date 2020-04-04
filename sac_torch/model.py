76f03db6f222452f50604969b2832c10602223bb
nutorbit <nutorbitx@gmail.com>
2020-04-04 15:21:39 +0700

    add model for image

--- sac_torch/model.py
+++ sac_torch/model.py
@@ -1,3 +1,4 @@
+import math
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -88,3 +89,82 @@ class Actor(BaseModel):
         self.action_scale = self.action_scale.to(device)
         self.action_bias = self.action_bias.to(device)
         return super().to(device)
+
+
+class ActorCNN(Actor):
+    def __init__(self, obs_dim, act_dim, action_space=None):
+        super().__init__(obs_dim, act_dim, action_space)
+        self.cnn = ImpalaCNN(obs_dim)
+        sizes = [256, 256, 256]
+        self.hidden = make_mlp(sizes, activation=nn.ReLU, output_activation=nn.ReLU)
+
+    def forward(self, obs):
+        x = self.cnn(obs/255)
+        x = self.hidden(x)
+        mean = self.mean(x)
+        log_std = self.log_std(x)
+        log_std = torch.clamp(log_std, min=-20, max=2)
+        return mean, log_std
+
+
+class CriticCNN(Critic):
+    def __init__(self, obs_dim, act_dim):
+        super().__init__(obs_dim, act_dim)
+        self.cnn = ImpalaCNN(obs_dim)
+        sizes = [256 + act_dim] + [256, 256, 256] + [1]
+        self.q1 = make_mlp(sizes=sizes, activation=nn.ReLU)
+        self.q2 = make_mlp(sizes=sizes, activation=nn.ReLU)
+
+    def forward(self, obs, act):
+        x = self.cnn(obs/255).view((obs.shape[0], -1))  # flatten
+        concat = torch.cat([x, act], dim=1)
+
+        return self.q1(concat), self.q2(concat)
+
+
+class ImpalaCNN(nn.Module):
+    """
+    The CNN architecture used in the IMPALA paper.
+    Ref: https://arxiv.org/abs/1802.01561
+    """
+
+    def __init__(self, image_size, depth_in=1):
+        super().__init__()
+        layers = []
+        for depth_out in [16, 32, 32]:
+            layers.extend([
+                nn.Conv2d(depth_in, depth_out, 3, padding=1),
+                nn.MaxPool2d(3, stride=2, padding=1),
+                ImpalaResidual(depth_out),
+                ImpalaResidual(depth_out),
+            ])
+            depth_in = depth_out
+        self.conv_layers = nn.Sequential(*layers)
+        self.linear = nn.Linear(math.ceil(image_size / 8) ** 2 * depth_in, 256)
+
+    def forward(self, x):
+        x = x.permute(0, 3, 1, 2).contiguous()
+        x = self.conv_layers(x)
+        x = F.relu(x)
+        x = x.view(x.shape[0], -1)
+        x = self.linear(x)
+        x = F.relu(x)
+        return x
+
+
+class ImpalaResidual(nn.Module):
+    """
+    A residual block for an IMPALA CNN.
+    """
+
+    def __init__(self, depth):
+        super().__init__()
+        self.conv1 = nn.Conv2d(depth, depth, 3, padding=1)
+        self.conv2 = nn.Conv2d(depth, depth, 3, padding=1)
+
+    def forward(self, x):
+        out = F.relu(x)
+        out = self.conv1(out)
+        out = F.relu(out)
+        out = self.conv2(out)
+        return out + x
