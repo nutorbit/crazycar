c6b057093fab8e85c21de581e319752ccef147f2
nutorbit <nutorbitx@gmail.com>
2020-04-11 02:11:49 +0700

    add set seed

--- sac_torch/utils.py
+++ sac_torch/utils.py
@@ -10,14 +10,12 @@ from datetime import datetime
 from torch.utils.tensorboard import SummaryWriter
 
 
-def set_seed(seed=100):
+def set_seed_everywhere(seed):
     torch.manual_seed(seed)
-    torch.cuda.manual_seed_all(seed)
-    torch.backends.cudnn.deterministic = True
-    torch.backends.cudnn.benchmark = False
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed_all(seed)
     np.random.seed(seed)
     random.seed(seed)
-    torch.backends.cudnn.enabled = False
 
 
 def huber_loss(x, delta=10.):
